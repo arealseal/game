acb5b1dba9dbf3ffeeb9d8869984bfaf24c4b06e
ARealSeal <53285975+arealseal@users.noreply.github.com>
2021-07-06 10:57:50 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+#Setting up Pygame
+import pygame
+pygame.init()
+win = pygame.display.set_mode((600,600))
+pygame.display.set_caption("Alex's Game")
+
+#attributes for cursor
+x = 200
+y = 590
+width = 200
+height = 10
+
+def quit_check():
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			return True
+
+# configure controls
+def left_check(a):
+	return a[pygame.K_LEFT]
+def right_check(a):
+	return a[pygame.K_RIGHT]
+def activate_check(a):
+	return a[pygame.K_SPACE]
+
+#main game loop
+run = True
+while run:
+	#makeshift clock in ms
+	#pygame.time.delay(1000//60)
+
+	#check for quit
+	if quit_check():
+		run = False
+		exit
+	
+	#set character position
+	keys = pygame.key.get_pressed()
+	if left_check(keys) and not right_check(keys):
+		x = 0
+	elif right_check(keys) and not left_check(keys):
+		x = 400
+	else:
+		x = 200
+	
+	#Draw lanes
+	pygame.draw.rect(win,(75,0,0),(0,0,200,600))
+	pygame.draw.rect(win,(0,75,0),(200,0,200,600))
+	pygame.draw.rect(win,(0,0,75),(400,0,200,600))
+
+	#Draw character
+	if not keys[pygame.K_SPACE]:
+		pygame.draw.rect(win,(200,200,200),(x,y,width,height))
+	else:
+		pygame.draw.rect(win,(200,250,250),(x,y,width,height))
+
+	#Update screen
+	pygame.display.update()
+pygame.quit
