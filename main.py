58a3ba1a0cffb42ef98df7f9198464d802032b43
ARealSeal <ajhahm27@gmail.com>
2021-07-08 14:18:44 -0700

    Saving before trying something pretty different.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import csv
 pygame.init()
 win = pygame.display.set_mode((600,600))
 pygame.display.set_caption("Alex's Game")
+framerate=10 #how many times the main loop should run in a second
 
 #Returns a set from the csv with the name entered
 def prepareCSV(filename):
@@ -14,6 +15,9 @@ def prepareCSV(filename):
 		for row in a:
 			b.append([int(x) for x in row])
 	return(b)
+
+def display_obstacle(lane,y):
+	pygame.draw.rect(win,(100,100,100),((200*lane)-200,y,200,10))
 
 #attributes for cursor
 x = 200
@@ -44,11 +48,23 @@ while not validInput:
 		validInput=False
 print("Loading Game Data.")
 
+# Creation of variables related to the level
+rowCount=gameData[0][0] #Amount of rows in the level
+rowPace=gameData[0][1] #Amount of rows per minute
+startRow=1 #First row that hasn't come and gone yet
+workingRow = 0 #Row that is being calculated
+rowFrametime = framerate*60/rowPace #How many frames any one row will be on screen for
+rowSpeed = rowFrametime//600 #How many pixels a row moves in a frame
+frameRemainder = 0 #How many frames have passed since startRow has changed.
+rowsVisible = rowSpeed
+
 #main game loop
 run = True
+frameCounter = 0
 while run:
 	#makeshift clock in ms
-	#pygame.time.delay(1000//60)
+	pygame.time.delay(1000//framerate)
+	frameCounter+=1
 
 	#check for quit
 	if quit_check():
@@ -75,6 +91,21 @@ while run:
 	else:
 		pygame.draw.rect(win,(200,250,250),(x,y,width,height))
 
+	#Should an obstacle be drawn?
+	frameRemainder+=1
+	if frameRemainder == rowFrametime:
+		startRow+=1
+	for rowPlus in range(startRow-1,rowPace-1): #number of rows visible given all rows are filled
+		workingRow = startRow+rowPlus #the row in the list that is being looked at
+		for lane in range(1,3):
+			#Don't try to render rows after the last one
+			if workingRow >= rowCount:
+				run = False
+				exit()
+			if gameData[workingRow][lane-1] == 1:
+				display_obstacle(lane,frameCounter*10)
+	
 	#Update screen
 	pygame.display.update()
+
 pygame.quit
